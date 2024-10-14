1258c8ec539ad139a66faed219899069e32c7313
worthlane <art.maklakov28@gmail.com>
2024-10-14 03:49:08 +0300

    fixes

--- contest1/B.cpp
+++ contest1/B.cpp
@@ -6,22 +6,35 @@
 
 static const ll MODULE = 1000003;
 
+template <typename T>
 class Matrix {
  public:
-  Matrix(const size_t height, const size_t width, std::vector<ll> values)
-      : m_data_(values) {
-    height_ = height;
-    width_ = width;
+  Matrix(std::vector<std::vector<T>> values) : data_(values)
+  {
+    height_ = values.size();
+    width_  = values[0].size();
   }
-  ~Matrix() { m_data_.clear(); }
 
-  std::vector<ll> m_data_;
-  size_t height_;
-  size_t width_;
+  Matrix(const size_t height, const size_t width) :
+        data_(height, std::vector<T>(width, 0)), height_(height), width_(width)
+  {}
+
+  ~Matrix() { data_.clear(); }
+
+  Matrix<T> operator*(const Matrix<T> &b);
+  Matrix<T> operator^(const T degree);
+  Matrix<T> operator=(const Matrix<T> &other);
+  std::vector<T>& operator[](const size_t index);
+
+  std::vector<std::vector<T>> get_data()   const { return data_; }
+  size_t                      get_height() const { return height_; }
+  size_t                      get_width()  const { return width_; }
+
+  private:
+    std::vector<std::vector<T>> data_;
+    size_t height_;
+    size_t width_;
 };
-
-Matrix operator*(const Matrix &a, const Matrix &b);
-Matrix operator^(const Matrix &a, const ll degree);
 
 int main() {
   ll n = 0;
@@ -30,60 +43,72 @@ int main() {
 
   n--;
 
-  Matrix ans = {5, 1, {0, 0, 0, 0, 1}};
-  Matrix step = {5, 5, {0, 1, 0, 0, 0,
-                        0, 0, 1, 0, 0,
-                        0, 0, 0, 1, 0,
-                        0, 0, 0, 0, 1,
-                        1, 1, 1, 1, 1}};
+  Matrix<ll> ans  = {{{0}, {0}, {0}, {0}, {1}}};
+  Matrix<ll> step = {{{0, 1, 0, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 0, 1, 0},
+                    {0, 0, 0, 0, 1},
+                    {1, 1, 1, 1, 1}}};
 
   ans = (step ^ n) * ans;
 
-  printf("%d\n", ans.m_data_[4] % MODULE);
-
-  // std::cout << ans.m_data_[4] % MODULE << std::endl;
-
+  std::cout << ans[4][0] % MODULE << std::endl;
+  
   return 0;
 }
 
-Matrix operator*(const Matrix &a, const Matrix &b) {
-  Matrix ret = {a.height_, b.width_, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+template <typename T> Matrix<T> Matrix<T>::operator*(const Matrix &b) {
+  Matrix result = {this->get_height(), b.get_width()};
 
-  assert(a.width_ == b.height_);
+  assert(this->get_width() == b.get_height());
 
-  for (int i = 0; i < ret.width_; i++) {
-    for (int j = 0; j < ret.height_; j++) {
-      for (int k = 0; k < a.width_; k++) {
-        ret.m_data_[i + (j * ret.width_)] +=
-            (a.m_data_[k + (j * a.width_)] * b.m_data_[i + (k * b.width_)]) %
-            MODULE;
+  for (int i = 0; i < result[0].size(); i++) {
+    for (int j = 0; j < result.get_data().size(); j++) {
+      for (int k = 0; k < (*this)[0].size(); k++) {
+        result[j][i] += ((*this)[j][k] * (b.get_data())[k][i]) % MODULE;
       }
     }
   }
 
-  return ret;
+  return result;
 }
 
-Matrix operator^(const Matrix &a, const ll degree) {
-  assert(a.width_ == a.height_);
+template <typename T> Matrix<T> Matrix<T>::operator^(const T degree) {
+
+  assert(this->get_width() == this->get_height());
 
   if (degree == 0) {
-    Matrix ONE = {a.height_, a.width_, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-    for (size_t i = 0; i < a.width_; i++) {
-      for (size_t j = 0; j < a.height_; j++) {
-        if (i == j) ONE.m_data_[i + (j * a.width_)] = 1;
+    Matrix one = {this->get_height(), this->get_width()};
+
+    for (size_t i = 0; i < this->get_width(); i++) {
+      for (size_t j = 0; j < this->get_height(); j++) {
+        if (i == j) one[j][i] = 1;
       }
     }
 
-    return ONE;
+    return one;
   } else if (degree == 1)
-    return a;
+      return *this;
 
-  Matrix ans = a ^ (degree / 2);
+  Matrix ans = *this ^ (degree / 2);
   ans = ans * ans;
-  if (degree % 2 == 1) ans = ans * a;
+
+  if (degree % 2 == 1)
+    ans = ans * *this;
+
   return ans;
+}
+
+template <typename T> Matrix<T> Matrix<T>::operator=(const Matrix<T> &other)
+{
+  this->data_ = other.get_data();
+  this->height_ = other.get_height();
+  this->width_ = other.get_width();
+
+  return *this;
+}
+template <typename T> std::vector<T>& Matrix<T>::operator[](const size_t index)
+{
+  return this->data_[index];
 }
