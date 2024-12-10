037cef078beb2aed73c2cd335f4e1123d9ee2073
worthlane <art.maklakov28@gmail.com>
2024-12-10 12:20:17 +0300

    after review

--- contest3/J.cpp
+++ contest3/J.cpp
@@ -8,28 +8,23 @@
 #include <unordered_set>
 #include <vector>
 
-#define ll long long
-
 struct Path {
-  std::string condition;
+  std::string state;
   std::string path;
 };
 
-struct Result {
-  ll len;
-  std::string path;
-};
+static const std::string NONE = "NONE";
 
-const size_t FIELD_SIZE = 3;
-const size_t FIELD_SIZE2 = FIELD_SIZE * FIELD_SIZE;
+const size_t FIELD_WIDTH = 3;
+const size_t FIELD_SIZE = FIELD_WIDTH * FIELD_WIDTH;
 const std::string ANSWER = "123456780";
 
 const char MOVE[] = {'L', 'R', 'U', 'D'};
-const ll DELTA_X[] = {-1, 1, 0, 0};
-const ll DELTA_Y[] = {0, 0, -1, 1};
+const int64_t DELTA_X[] = {-1, 1, 0, 0};
+const int64_t DELTA_Y[] = {0, 0, -1, 1};
 
-Result bfs(std::string& start) {
-  if (start == ANSWER) return {0, ""};
+std::string bfs_recover_path(std::string& start) {
+  if (start == ANSWER) return "";
 
   std::unordered_set<std::string> visited;
   std::queue<Path> q;
@@ -39,22 +34,22 @@ Result bfs(std::string& start) {
     Path element = q.front();
     q.pop();
 
-    std::string current_state = element.condition;
+    std::string current_state = element.state;
     std::string current_path = element.path;
 
     size_t free_cell = current_state.find('0');
-    size_t free_y = free_cell / FIELD_SIZE;
-    size_t free_x = free_cell % FIELD_SIZE;
+    size_t free_y = free_cell / FIELD_WIDTH;
+    size_t free_x = free_cell % FIELD_WIDTH;
 
     for (size_t delta = 0; delta < 4; delta++) {
       size_t new_x = DELTA_X[delta] + free_x;
       size_t new_y = DELTA_Y[delta] + free_y;
 
-      if (new_y < 0 || new_y >= FIELD_SIZE) continue;
+      if (new_y < 0 || new_y >= FIELD_WIDTH) continue;
 
-      if (new_x < 0 || new_x >= FIELD_SIZE) continue;
+      if (new_x < 0 || new_x >= FIELD_WIDTH) continue;
 
-      size_t next_cell = new_y * FIELD_SIZE + new_x;
+      size_t next_cell = new_y * FIELD_WIDTH + new_x;
 
       std::string next_state = current_state;
       std::swap(next_state[free_cell], next_state[next_cell]);
@@ -62,7 +57,7 @@ Result bfs(std::string& start) {
       Path next = {next_state, current_path + MOVE[delta]};
 
       if (next_state == ANSWER)
-        return {static_cast<ll>(next.path.length()), next.path};
+        return next.path;
 
       if (visited.find(next_state) == visited.end()) {
         visited.insert(next_state);
@@ -71,13 +66,13 @@ Result bfs(std::string& start) {
     }
   }
 
-  return {-1, ""};
+  return NONE;
 }
 
 int main() {
   std::string start;
 
-  for (size_t i = 0; i < FIELD_SIZE2; i++) {
+  for (size_t i = 0; i < FIELD_SIZE; i++) {
     int a = 0;
 
     std::cin >> a;
@@ -85,10 +80,16 @@ int main() {
     start += '0' + a;
   }
 
-  Result ans = bfs(start);
+  std::string ans = bfs_recover_path(start);
 
-  std::cout << ans.len << std::endl;
-  std::cout << ans.path << std::endl;
+  if (ans == NONE)
+  {
+    std::cout << -1 << std::endl;
+    return 0;
+  }
+
+  std::cout << ans.length() << std::endl;
+  std::cout << ans << std::endl;
 
   return 0;
 }
