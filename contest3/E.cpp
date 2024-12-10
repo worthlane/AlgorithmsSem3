2e34c99bba9bb908b3b79cdba05ece18496151ad
worthlane <art.maklakov28@gmail.com>
2024-12-10 12:25:16 +0300

    minor fixes

--- contest3/E.cpp
+++ contest3/E.cpp
@@ -6,68 +6,66 @@
 #include <iostream>
 #include <vector>
 
-#define ll long long
+enum Color { NotVisited, InProgress, Visited };
 
-enum Color { WHITE, GRAY, BLACK };
-
-void dfs_recursive(std::vector<std::vector<ll>>& graph, const ll start,
-                   std::vector<Color>& colors, std::vector<ll>& order) {
-  colors[start] = GRAY;
+void dfs_recursive(std::vector<std::vector<int64_t>>& graph, const int64_t start,
+                   std::vector<Color>& colors, std::vector<int64_t>& order) {
+  colors[start] = InProgress;
   for (size_t neighbour : graph[start]) {
-    if (colors[neighbour] == WHITE)
+    if (colors[neighbour] == NotVisited)
       dfs_recursive(graph, neighbour, colors, order);
   }
-  colors[start] = BLACK;
+  colors[start] = Visited;
   order.push_back(start);
 }
 
-void dfs_fill_component(std::vector<std::vector<ll>>& graph, const ll start,
-                        std::vector<Color>& colors, std::vector<ll>& components,
-                        const ll curr_component) {
-  colors[start] = GRAY;
+void dfs_fill_component(std::vector<std::vector<int64_t>>& graph, const int64_t start,
+                        std::vector<Color>& colors, std::vector<int64_t>& components,
+                        const int64_t curr_component) {
+  colors[start] = InProgress;
   for (size_t neighbour : graph[start]) {
-    if (colors[neighbour] == WHITE)
+    if (colors[neighbour] == NotVisited)
       dfs_fill_component(graph, neighbour, colors, components, curr_component);
   }
-  colors[start] = BLACK;
+  colors[start] = Visited;
   components[start] = curr_component;
 }
 
 int main() {
-  size_t N = 0, M = 0;
+  size_t vertexes_amt = 0, edges_amt = 0;
 
-  std::cin >> N >> M;
+  std::cin >> vertexes_amt >> edges_amt;
 
-  std::vector<std::vector<ll>> graph(N);
-  std::vector<std::vector<ll>> inverse_graph(N);
+  std::vector<std::vector<int64_t>> graph(vertexes_amt);
+  std::vector<std::vector<int64_t>> inverse_graph(vertexes_amt);
 
-  for (size_t i = 0; i < M; i++) {
-    ll a = 0, b = 0;
-    std::cin >> a >> b;
-    a--;
-    b--;
-    graph[a].push_back(b);
-    inverse_graph[b].push_back(a);
+  for (size_t i = 0; i < edges_amt; i++) {
+    int64_t start = 0, end = 0;
+    std::cin >> start >> end;
+    start--;
+    end--;
+    graph[start].push_back(end);
+    inverse_graph[end].push_back(start);
   }
 
-  std::vector<Color> colors(N, WHITE);
-  std::vector<ll> order;
+  std::vector<Color> colors(vertexes_amt, NotVisited);
+  std::vector<int64_t> order;
 
-  for (size_t i = 0; i < N; i++) {
-    if (colors[i] == WHITE) {
+  for (size_t i = 0; i < vertexes_amt; i++) {
+    if (colors[i] == NotVisited) {
       dfs_recursive(graph, i, colors, order);
     }
   }
 
-  std::vector<ll> components(N, 0);
-  std::vector<Color> inverse_colors(N, WHITE);
+  std::vector<int64_t> components(vertexes_amt, 0);
+  std::vector<Color> inverse_colors(vertexes_amt, NotVisited);
 
-  ll curr_component = 1;
+  int64_t curr_component = 1;
 
-  for (ll i = N - 1; i >= 0; i--) {
-    ll elem = order[i];
+  for (int64_t i = vertexes_amt - 1; i >= 0; i--) {
+    int64_t elem = order[i];
 
-    if (inverse_colors[elem] == WHITE) {
+    if (inverse_colors[elem] == NotVisited) {
       dfs_fill_component(inverse_graph, elem, inverse_colors, components,
                          curr_component);
       curr_component++;
@@ -76,7 +74,7 @@ int main() {
 
   std::cout << curr_component - 1 << std::endl;
 
-  for (ll elem : components) {
+  for (int64_t elem : components) {
     std::cout << elem << " ";
   }
   std::cout << std::endl;
