764d70c52c1f43d32a0d76c799bfcdee00ac266c
worthlane <art.maklakov28@gmail.com>
2024-12-22 21:15:59 +0300

    after review

--- contest3/E.cpp
+++ contest3/E.cpp
@@ -8,19 +8,65 @@
 
 enum Color { NotVisited, InProgress, Visited };
 
+class Graph
+{
+public:
+  Graph(const size_t vertexes_amt, const size_t edges_amt) : vertexes_amt_(vertexes_amt), edges_amt_(edges_amt), graph_(vertexes_amt) {}
+
+  friend std::istream& operator>>(std::istream& in, Graph& graph);
+
+  size_t get_vertexes_amt() const { return vertexes_amt_; }
+  size_t get_edges_amt() const { return edges_amt_; }
+
+  std::vector<int64_t>& operator[](const size_t index) { return graph_[index]; }
+  const std::vector<int64_t>& operator[](const size_t index) const { return graph_[index]; }
+
+  Graph get_inverse_graph() const;
+
+private:
+  std::vector<std::vector<int64_t>> graph_;
+  size_t vertexes_amt_;
+  size_t edges_amt_;
+};
+
+Graph Graph::get_inverse_graph() const {
+  Graph inverse_graph(vertexes_amt_, edges_amt_);
+
+  for (size_t i = 0; i < vertexes_amt_; i++) {
+    for (size_t j = 0; j < graph_[i].size(); j++) {
+      inverse_graph[graph_[i][j]].push_back(i);
+    }
+  }
+
+  return inverse_graph;
+}
+
+std::istream& operator>>(std::istream& in, Graph& graph) {
+  for (size_t i = 0; i < graph.edges_amt_; i++) {
+    int64_t start = 0, end = 0;
+    std::cin >> start >> end;
+
+    start--;
+    end--;
+
+    graph.graph_[start].push_back(end);
+  }
+  return in;
+}
+
 class StrongComponentSearcher
 {
 public:
-  StrongComponentSearcher(const size_t vertexes_amt);
+  StrongComponentSearcher(const size_t vertexes_amt, Graph& graph);
 
-  void get_edges(const size_t edges_amt);
   void find_strong_components();
 
   std::vector<int64_t> get_components() const { return components_; }
   int64_t              get_components_amt() const { return curr_component_ - 1; }
+
 private:
-  std::vector<std::vector<int64_t>> graph_;
-  std::vector<std::vector<int64_t>> inverse_graph_;
+  Graph graph_;
+  Graph inverse_graph_;
   std::vector<Color> colors_;
   std::vector<Color> inverse_colors_;
   std::vector<int64_t> components_;
@@ -50,24 +96,13 @@ void StrongComponentSearcher::dfs_inverse_fill_component(const int64_t start) {
   components_[start] = curr_component_;
 }
 
-StrongComponentSearcher::StrongComponentSearcher(const size_t vertexes_amt) :
+StrongComponentSearcher::StrongComponentSearcher(const size_t vertexes_amt, Graph& graph) :
   colors_(vertexes_amt, NotVisited), inverse_colors_(vertexes_amt, NotVisited),
-  components_(vertexes_amt), graph_(vertexes_amt), inverse_graph_(vertexes_amt)
-{
-}
-
-void StrongComponentSearcher::get_edges(const size_t edges_amt) {
-  for (size_t i = 0; i < edges_amt; i++) {
-    int64_t start = 0, end = 0;
-    std::cin >> start >> end;
-    start--; end--;
-    graph_[start].push_back(end);
-    inverse_graph_[end].push_back(start);
-  }
-}
+  components_(vertexes_amt), graph_(graph), inverse_graph_(graph.get_inverse_graph())
+{}
 
 void StrongComponentSearcher::find_strong_components() {
-  size_t vertexes_amt = graph_.size();
+  size_t vertexes_amt = graph_.get_vertexes_amt();
   std::vector<int64_t> order;
 
   for (size_t i = 0; i < vertexes_amt; i++) {
@@ -93,13 +128,15 @@ int main() {
 
   std::cin >> vertexes_amt >> edges_amt;
 
-  StrongComponentSearcher searcher(vertexes_amt);
-  searcher.get_edges(edges_amt);
+  Graph graph(vertexes_amt, edges_amt);
+  std::cin >> graph;
+
+  StrongComponentSearcher searcher(vertexes_amt, graph);
   searcher.find_strong_components();
 
   int64_t components_amt = searcher.get_components_amt();
-  std::vector<int64_t> components = searcher.get_components();
 
+  std::vector<int64_t> components = searcher.get_components();
   std::cout << components_amt << std::endl;
 
   for (int64_t elem : components) {
