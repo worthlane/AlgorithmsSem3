5937f290435a631298b0769da9fadc7838f1d90a
worthlane <art.maklakov28@gmail.com>
2024-12-22 17:47:12 +0300

    after review

--- contest5/F.cpp
+++ contest5/F.cpp
@@ -8,37 +8,74 @@
 #include <set>
 #include <vector>
 
-static const int INF = 1e9;
+class Graph
+{
+public:
+  Graph(size_t size) : graph_(size, std::vector<bool>(size, false)), size_(size) {}
+  Graph(const Graph& other) : graph_(other.graph_), size_(other.size_) {}
 
-int main() {
-  int N = 0;
+  friend std::istream& operator>>(std::istream& in, Graph& graph) {
+    for (size_t i = 0; i < graph.size_; i++) {
+      for (size_t j = 0; j < graph.size_; j++) {
+        bool connected = false;
+        in >> connected;
 
-  std::cin >> N;
-
-  std::vector<std::vector<bool>> graph(N, std::vector<bool>(N, false));
-  for (size_t i = 0; i < N; i++) {
-    for (size_t j = 0; j < N; j++) {
-      bool connected = false;
-      std::cin >> connected;
-
-      if (connected) graph[i][j] = true;
+        if (connected) graph.graph_[i][j] = connected;
+      }
     }
+    return in;
   }
 
-  for (size_t k = 0; k < N; k++) {
-    for (size_t i = 0; i < N; i++) {
-      for (size_t j = 0; j < N; j++) {
-        graph[i][j] = graph[i][j] || (graph[i][k] && graph[k][j]);
+  friend std::ostream& operator<<(std::ostream& out, Graph& graph) {
+    for (size_t i = 0; i < graph.size_; i++) {
+
+      for (size_t j = 0; j < graph.size_; j++) {
+        out << graph.graph_[i][j] << " ";
+      }
+
+      out << std::endl;
+    }
+
+    return out;
+  }
+
+  size_t get_size() const { return size_; }
+
+  std::vector<bool>& operator[](const size_t index) { return graph_[index]; }
+
+private:
+  std::vector<std::vector<bool>> graph_;
+  size_t size_;
+};
+
+Graph get_transitive_closure(const Graph& graph)
+{
+  Graph transitive_closure(graph);
+
+  size_t size = graph.get_size();
+
+  for (size_t k = 0; k < size; k++) {
+    for (size_t i = 0; i < size; i++) {
+      for (size_t j = 0; j < size; j++) {
+        transitive_closure[i][j] = transitive_closure[i][j] || (transitive_closure[i][k] && transitive_closure[k][j]);
       }
     }
   }
 
-  for (size_t i = 0; i < N; i++) {
-    for (size_t j = 0; j < N; j++) {
-      std::cout << graph[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
+  return transitive_closure;
+}
+
+int main() {
+  int rooms_amount = 0;
+  std::cin >> rooms_amount;
+
+  Graph graph(rooms_amount);
+
+  std::cin >> graph;
+
+  Graph res = get_transitive_closure(graph);
+
+  std::cout << res;
 
   return 0;
 }
